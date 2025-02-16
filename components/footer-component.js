c3d93b88133e4ffbf47bd7f06bef2e0d365a051e
fumiya-kume <fumiya.kume@hotmail.com>
2025-02-16 17:37:14 +0900

    refactor: Update components to use shared styles

--- components/footer-component.js
+++ components/footer-component.js
@@ -1,41 +1,26 @@
 import { BaseComponent } from './base-component.js';
 
 export class FooterComponent extends BaseComponent {
-  constructor() {
-    super();
-    this.loadTailwindCSS();
-  }
-
-  connectedCallback() {
+  async connectedCallback() {
+    await super.connectedCallback();
     this.render();
   }
 
   render() {
     this.shadowRoot.innerHTML = `
-      <style>
-        :host {
-          display: block;
-          width: 100%;
-        }
-        
-        a {
-          transition: color 0.2s;
-        }
-      </style>
-      
-      <footer class="bg-white dark:bg-gray-800 shadow">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+      <footer class="bg-white shadow">
+        <div class="px-4 py-4">
           <div class="flex justify-between items-center">
-            <p class="text-gray-600 dark:text-gray-400">
+            <p class="text-gray-600">
               <a href="https://github.com/fumiya-kume/presentation" 
-                 class="hover:text-gray-900 dark:hover:text-white">
+                 class="hover:text-gray-900">
                 <i class="fab fa-github mr-2"></i>
                 View on GitHub
               </a>
             </p>
-            <p class="text-gray-500 dark:text-gray-400 text-sm">
+            <p class="text-gray-500 text-sm">
               <a href="https://github.com/marp-team/marp" 
-                 class="hover:text-gray-900 dark:hover:text-white">
+                 class="hover:text-gray-900">
                 Built with Marp
               </a>
             </p>
